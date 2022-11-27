d6ed8dc53c1d8590bbcc1d2f437a5606849b65b5
Thimira Nirmal <timnirmal@gmail.com>
2022-11-27 15:41:54 +0530

    SM icon GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import SocialMedia from "./component/SocialMedia";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App bg-black">
+            <div className="text-white">
 
-            <h1 className="">Hello I’m 👋</h1>
-            <h1>Thimira Nirmal</h1>
-            <span>Web Developer 🧑‍💻</span>
-            <span>Data Science 📈</span>
-            <span>Machine Learning 🤖</span>
+                <h1 className="">Hello I’m 👋</h1>
+                <h1>Thimira Nirmal</h1>
+                <span>Web Developer 🧑‍💻</span>
+                <span>Data Science 📈</span>
+                <span>Machine Learning 🤖</span>
 
-            <SocialMedia/>
+                <SocialMedia/>
+            </div>
+
         </div>
     )
 }
--- src/component/SocialMedia/index.jsx
+++ src/component/SocialMedia/index.jsx
@@ -1,18 +1,33 @@
 import {Link} from "react-router-dom";
 import {me} from "../../data";
 
+import iconGitHub from "/images/icon _github.png";
+import iconLinkedIn from "/images/icon _linkedin.png";
+import iconTwitter from "/images/icon _twitter.png";
+import iconFacebook from "/images/icon _facebook.png";
+import iconEmail from "/images/icon _email.png";
+
+
 function SocialMedia() {
+    console.log(me.social.github.url)
+
     return (
-        <nav className="navbar">
-            <div className="navbar__links">
-                <ul>
-                    <Link to={me.social.github} target="_blank">Github</Link>
-                </ul>
-            </div>
-            <div className="navbar__resume">
-                {null}
-            </div>
-        </nav>
+        <div className="social-media">
+            <h1>social media</h1>
+            <ul>
+                {me.social.github && (
+                    <li>
+                        <Link to={me.social.github}>
+                            <img
+                                src={iconGitHub}
+                                alt="Image Link"
+                                width="50px"
+                            />
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </div>
     )
 }
 
