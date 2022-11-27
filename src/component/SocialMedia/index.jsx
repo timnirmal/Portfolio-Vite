631638c152f3d805a852c782d219f3f00aa8ab40
Thimira Nirmal <timnirmal@gmail.com>
2022-11-27 16:21:44 +0530

    SM Added

--- src/component/SocialMedia/index.jsx
+++ src/component/SocialMedia/index.jsx
@@ -7,26 +7,45 @@ import iconTwitter from "/images/icon _twitter.png";
 import iconFacebook from "/images/icon _facebook.png";
 import iconEmail from "/images/icon _email.png";
 
+import {FaGithub} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+import {FaTwitter} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa";
+import {AiFillMail as FaEmail} from "react-icons/ai"
 
 function SocialMedia() {
     console.log(me.social.github.url)
 
     return (
         <div className="social-media">
-            <h1>social media</h1>
-            <ul>
-                {me.social.github && (
-                    <li>
-                        <Link to={me.social.github}>
-                            <img
-                                src={iconGitHub}
-                                alt="Image Link"
-                                width="50px"
-                            />
-                        </Link>
-                    </li>
-                )}
-            </ul>
+            {me.social.github && (
+                <a href={me.social.github.url} target="_blank" rel="noreferrer"
+                   className="inline-block"
+                >
+                    <FaGithub className="text-5xl"/>
+                </a>
+            )}
+            {me.social.linkedin && (
+                <a href={me.social.linkedin.url} target="_blank" rel="noreferrer" className="inline-block">
+                    <FaLinkedin className="text-5xl"/>
+                </a>
+            )}
+            {me.social.twitter && (
+                <a href={me.social.twitter.url} target="_blank" rel="noreferrer" className="inline-block">
+                    <FaTwitter className="text-5xl"/>
+                </a>
+            )}
+            {me.social.facebook && (
+                <a href={me.social.facebook.url} target="_blank" rel="noreferrer" className="inline-block">
+                    <FaFacebook className="text-5xl"/>
+                </a>
+            )}
+            {me.email && (
+                <a href={`mailto:${me.email}`} target="_blank" rel="noreferrer" className="inline-block">
+                    <FaEmail className="text-5xl"/>
+                </a>
+            )}
+
         </div>
     )
 }
